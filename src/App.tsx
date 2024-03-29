4fccdcbc0e971ccbba64cf09cc7b6a2ff9f94476
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2024-03-29 14:23:20 -0400

    reafactor: Separate buttons from app

--- src/App.tsx
+++ src/App.tsx
@@ -4,97 +4,14 @@ import { Button } from "./components/Button/Button";
 import { useTheme } from "./hooks/useTheme";
 import { IconSun } from "./components/Icons/IconSun";
 import { IconMoon } from "./components/Icons/IconMoon";
+import { buttons } from "./config/buttons";
 
-function App() {
-  type Info = {
-    text: string;
-    class: string;
-  };
+export type ButtonMetadata = {
+  text: string;
+  class: string;
+};
 
-  const buttons: Info[] = [
-    {
-      text: "%",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "C",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "←",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "÷",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "1",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "2",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "3",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "×",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "4",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "5",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "6",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "-",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "7",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "8",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "9",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: "+",
-      class: "text-secondary bg-primary dark:bg-slate-900",
-    },
-    {
-      text: "±",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-900",
-    },
-    {
-      text: "0",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
-    },
-    {
-      text: ".",
-      class: "text-secondary bg-[#edf9ff] dark:bg-slate-900",
-    },
-    {
-      text: "=",
-      class:
-        "text-white font-semibold m-3 bg-secondary hover:bg-blue-500 transition-all active:ring-4 dark:bg-blue-800",
-    },
-  ];
-
+const App = () => {
   const [expression, setExpression] = useState("");
   const [result, setResult] = useState("");
   const { theme, setTheme } = useTheme();
@@ -141,13 +58,15 @@ function App() {
       </button>
 
       <main className='w-[340px] mx-auto mt-14 rounded-xl overflow-hidden shadow-lg '>
-        <div className='text-white bg-secondary p-5 flex justify-end dark:bg-slate-950'>
+        <div className='text-white bg-secondary p-5 flex justify-end dark:bg-slate-950 tracking-wide'>
           {expression}
         </div>
+
         <div className='text-white bg-secondary p-5 flex justify-between dark:bg-slate-950'>
           <h1 className='text-3xl font-semibold'>=</h1>
           <h1 className='text-3xl font-semibold'>{result}</h1>
         </div>
+
         <div className='bg-white dark:bg-slate-800 p-5 h-[400px] grid grid-cols-4 '>
           {buttons.map((button) => (
             <Button
@@ -161,6 +80,6 @@ function App() {
       </main>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/config/buttons.ts
@@ -0,0 +1,85 @@
+import { ButtonMetadata } from "../App";
+
+export const buttons: ButtonMetadata[] = [
+  {
+    text: "%",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "C",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "←",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "÷",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "1",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "2",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "3",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "×",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "4",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "5",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "6",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "-",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "7",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "8",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "9",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: "+",
+    class: "text-secondary bg-primary dark:bg-slate-900",
+  },
+  {
+    text: "±",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-900",
+  },
+  {
+    text: "0",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-950",
+  },
+  {
+    text: ".",
+    class: "text-secondary bg-[#edf9ff] dark:bg-slate-900",
+  },
+  {
+    text: "=",
+    class:
+      "text-white font-semibold m-3 bg-secondary hover:bg-blue-500 transition-all active:ring-4 dark:bg-blue-800",
+  },
+];
